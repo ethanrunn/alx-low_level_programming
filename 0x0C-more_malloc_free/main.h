8da452757a64493bef10758484c98421c5d7c961
ethanrunn <ethanrunn5@gmail.com>
2022-10-08 17:26:48 -0700

    first commit

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,7 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int main(void);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
@@ -9,5 +8,6 @@ int *array_range(int min, int max);
 int _putchar(char c);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _puts(const char *str);
+int main(int argc, char **argv)
 
 #endif
